5416c732cece1f104e16a1382f21bdca90e184b9
victor67r <victor67ugr.com>
2026-04-08 16:08:14 +0200

    cambios boton musica

--- scripts/juego.js
+++ scripts/juego.js
@@ -3,3 +3,16 @@ function empezarJuego() {
   document.getElementById("pantallaInicio").style.display = "none";
   document.getElementById("pantallaJuego").style.display = "block";
 }
+
+function toggleMusica() {
+  const musica = document.getElementById("musicaFondo");
+  const boton = document.getElementById("botonMusica");
+
+  if (musica.muted) {
+    musica.muted = false;
+    boton.textContent = "🔊";
+  } else {
+    musica.muted = true;
+    boton.textContent = "🔇";
+  }
+}
