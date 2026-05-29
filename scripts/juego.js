a532361fd0b121afd222dfac8abfd08772a11761
victor67r <victor67ugr.com>
2026-05-29 10:17:59 +0200

    div

--- scripts/juego.js
+++ scripts/juego.js
@@ -686,11 +686,8 @@ function mostrarSello(texto) {
 function borrarRanking() {
 
   let confirmar = confirm("¿Seguro que quieres borrar todo el ranking?");
-
   if (!confirmar) return;
-
   localStorage.removeItem("ranking");
-
   cargarRankingPantallaInicio();
 }
 
